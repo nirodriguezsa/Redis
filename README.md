1b3264008b1df1c01a54b9d43beb41826a06c244
Nicolas Rodriguez <nicolas.salguero@rappi.com>
2022-10-18 12:19:36 -0500

    feat Add endpoint products, add response whit a local endpoint using json-server

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Redis
+
+## Init whit
+```npm i```
+
+### To run a fake api local
+
+```json-server --watch fake-api/response.json --port 3067```
+
+#### Api:
+
+http://localhost:3077/get/excuse/10
+
+http://localhost:3067/products/
+
--- src/index.ts
+++ src/index.ts
@@ -16,13 +16,23 @@ app.get('/health', (req, res) => {
 });
 
 app.get('/get/excuse/:number', async (req, res) => {
-  const excuse = await client.get(`excuse-No${req.params.number}`);
-  if(excuse) return res.json(JSON.parse(excuse));
+  const excuseCache = await client.get(`excuse-No${req.params.number}`);
+  if(excuseCache) return res.json(JSON.parse(excuseCache));
 
   console.log('Getting data from API');
-  const excuses = await axios.get('https://excuser.herokuapp.com/v1/excuse/id/'+req.params.number);
-  client.set(`excuse-No${req.params.number}`, JSON.stringify(excuses.data));
-  return res.json(excuses.data);
+  const excuse = await axios.get('https://excuser.herokuapp.com/v1/excuse/id/'+req.params.number);
+  client.set(`excuse-No${req.params.number}`, JSON.stringify(excuse.data));
+  return res.json(excuse.data);
+});
+
+app.get('/get-product/:id', async (req, res) => {
+  const productCache = await client.get(`product-No${req.params.id}`);
+  if(productCache) return res.json(JSON.parse(productCache));
+
+  console.log('Getting data from Fake-Api');
+  const product = await axios.get('http://localhost:3067/products/'+req.params.id);
+  client.set(`product-No${req.params.id}`, JSON.stringify(product.data));
+  return res.json(product.data);
 });
 
 
